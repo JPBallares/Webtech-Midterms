8160f3307aa08fe2aec3df45dafd2cd7a475422d
Dominic <pocaand@gmail.com>
2018-03-28 14:25:17 +0800

    Edit home page and add lessons.html

--- WebSite/js/scroll.js
+++ WebSite/js/scroll.js
@@ -1,19 +1,7 @@
-$(function() {
-   
-    var navOffset = $("nav").offset().top;
-    
-    var newNavOffset = navOffset + 62;
-    
-    $(window).scroll(function() {
-        var scrollPos = $(window).scrollTop();
-        
-        if (scrollPos >= newNavOffset) {
-            $("nav").addClass("fixed");
-            $("#main_content_container").css({paddingTop: '2.8%'});
-        } else {
-            $("nav").removeClass("fixed");
-            $("#main_content_container").css({paddingTop: '0%'});
-        }
-    })   
-    
-});
+var zero = 0;
+$(document).ready(function(){
+    $(window).on('scroll', function(){
+        $('.navbar').toggleClass('hide', $(window).scrollTop() > zero);
+        zero = $(window).scrollTop();
+    })
+})
---
+++ WebSite/js/sidemenu.js
@@ -0,0 +1,21 @@
+var c1 = document.getElementById("www");
+var c2 = document.getElementById("int");
+
+c1.addEventListener("click", showContent);
+c2.addEventListener("click", showContent);
+
+function showContent() {
+    var allContent = document.querySelectorAll("#lessons_main_content div");
+    
+    for (var i = 0; i < allContent.length; i++) {
+        allContent[i].className = "hide";
+    }
+    
+    var conId = this.attributes["data-con"].value;
+    var con = document.getElementById(conId);
+    if (con.className == "hide") {
+        con.className = "";
+    } else {
+        con.className = "hide";        
+    }
+}
